805ff343d4157ccb46c1733ea8bd05d87ae509f5
Jose Rodriguez <boriel@gmail.com>
2020-01-21 00:25:48 +0100

    Test line comments

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -6,4 +6,30 @@ from main import Lexer, LexException, TokenID, Token
 def test_correctly_skips_blanks():
     lex = Lexer(io.StringIO('  \n\r\t'))
     tok = lex.get_token()
-    assert tok == Token(TokenID.EOF, 1, 5, '')
+    assert tok.id_ == TokenID.EOF
+
+
+def test_skip_line_comment():
+    lex = Lexer(io.StringIO("""
+    // A skipped comment
+    """))
+    tok = lex.get_token()
+    assert tok.id_ == TokenID.EOF
+
+    lex = Lexer(io.StringIO("""
+    /// A skipped comment
+    """))
+    tok = lex.get_token()
+    assert tok.id_ == TokenID.EOF
+
+    lex = Lexer(io.StringIO("""
+    / // A skipped comment
+    """))
+    tok = lex.get_token()
+    assert tok.id_ == TokenID.DIV
+
+    lex = Lexer(io.StringIO("""
+    // A skipped comment
+    /"""))
+    tok = lex.get_token()
+    assert tok.id_ == TokenID.DIV
