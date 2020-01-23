1e210e2ccba1b3599c35a4aec91cd08d198eacf2
Jose Rodriguez <boriel@gmail.com>
2020-01-24 00:00:21 +0100

    Parse numerical expressions
    Makes the parser to parse numbers (both floats and integers) and assigns them a default (guessed) type.

--- parser.py
+++ parser.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from typing import Union, TextIO, List
+from typing import Union, TextIO, List, Optional
 from io import StringIO
 
 from lexer import Lexer, Token, TokenID, TOKEN_MAP
 import ast_
 from symbol_table import SymbolTable
+import log
 
 
 class ParserSyntaxErrorException(BaseException):
@@ -29,7 +30,7 @@ class Parser:
         self.lookahead = self.lex.get_token()
         self.output_stream = output_stream
         self.symbol_table = SymbolTable()
-        self._primitive_types = []
+        self.primitive_types = []
 
         # Populates symbol table
         self._declare_primitive_type()
@@ -38,49 +39,64 @@ class Parser:
         """ Declares primitive types
         """
         for type_name in ast_.PRIMITIVE_TYPES:
-            if type_name in ('str', 'char'):
-                continue  # Numerical types only for the moment
-
             token = Token(TOKEN_MAP[type_name], 0, 0, type_name)
-            if type_name.startswith('u'):  # Unsigned?
-                self.symbol_table.declare_symbol(token, ast_.UnsignedIntType(token))
-            else:
-                self.symbol_table.declare_symbol(token, ast_.SignedIntType(token))
 
+            if type_name in ('str', 'char', 'bool'):
+                type_ = ast_.PrimitiveScalarTypeAST(token)
+            elif type_name.startswith('u'):  # Unsigned?
+                type_ = ast_.UnsignedIntType(token)
+            else:
+                type_ = ast_.SignedIntType(token)
+
+            self.symbol_table.declare_symbol(token, type_)
+            self.primitive_types.append(type_)
 
     def parse(self):
         pass
 
-    def error(self, line, msg):
-        raise ParserSyntaxErrorException("{}: {}".format(line, msg))
+    @staticmethod
+    def error(line, msg):
+        log.error("{}: {}".format(line, msg))
 
-    def error_unexpected_token(self, token: Token):
+    def error_unexpected_token(self, token: Token = None):
+        if token is None:
+            token = self.lookahead
         self.error(token.line, "syntax error: unexpected token '{}'".format(repr(token)))
 
     def match(self, tokens: Union[TokenID, List[TokenID]]) -> List[Token]:
         if isinstance(tokens, TokenID):
-            tokens = [TokenID]
+            tokens = [tokens]
 
         result: List[Token] = []
         for tok in tokens:
             if self.lookahead.id_ == tok:
                 result.append(self.lookahead)
+                self.lookahead = self.lex.get_token()
             else:
-                self.error_unexpected_token(self.lookahead)
+                self.error_unexpected_token()
 
         return result
 
-    def match_number_literal(self) -> ast_.NumericLiteralAST:
+    def match_number_literal(self) -> Optional[ast_.NumericLiteralAST]:
         if self.lookahead == TokenID.INT_LITERAL:
-            result = self.match(TokenID.INT_LITERAL)[0]
-            for t in PRIMITIVE_INT_TYPES:
-                if t.min_val <= result.num_val <= t.max_val:
+            token = self.match(TokenID.INT_LITERAL)[0]
+            for t in self.primitive_types:
+                if not isinstance(t, ast_.IntTypeAST):
+                    continue
+
+                if t.min_val <= token.num_val <= t.max_val:
                     type_ = t
                     break
             else:
-                type_ = ast_.TypeFloatAST
+                type_ = self.symbol_table.resolve_symbol('float')
+        elif self.lookahead == TokenID.FLOAT_LITERAL:
+            token = self.match(TokenID.FLOAT_LITERAL)[0]
+            type_ = self.symbol_table.resolve_symbol('float')
         else:
-            type_ = ast_.TypeFloatAST
+            self.error_unexpected_token()
+            return None
 
-
+        result = ast_.NumericLiteralAST(token, type_)
+        self.lookahead = self.lex.get_token()
+        return result
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,8 +3,40 @@
 import io
 import parser
 
+import ast_
+from lexer import Token, TokenID
+import log
+
 
 def test_parser_init():
     parser_ = parser.Parser(io.StringIO(''))
+    assert [x.name for x in parser_.primitive_types] == ['int8', 'uint8', 'int32', 'uint32', 'int64',
+                                                         'uint64', 'float', 'bool', 'char', 'str']
 
 
+def test_parser_numeric_literal():
+    parser_ = parser.Parser(io.StringIO('  3452  '))
+    ast = parser_.match_number_literal()
+    assert isinstance(ast.type, ast_.IntTypeAST)
+    assert isinstance(ast.token, Token)
+    assert ast.token.id_ == TokenID.INT_LITERAL
+    assert ast.token.value == '3452'
+    assert ast.token.num_val == 3452
+
+
+def test_parser_numeric_literal_float():
+    parser_ = parser.Parser(io.StringIO('  .3452e2  '))
+    ast = parser_.match_number_literal()
+    assert isinstance(ast.type, ast_.IntTypeAST)
+    assert isinstance(ast.token, Token)
+    assert ast.token.id_ == TokenID.FLOAT_LITERAL
+    assert ast.token.value == '.3452e2'
+    assert ast.token.num_val == 34.52
+
+
+def test_parser_numeric_error(mocker):
+    mocker.patch('log.error')
+    parser_ = parser.Parser(io.StringIO('  a '))
+    ast = parser_.match_number_literal()
+    assert ast is None, "Syntax error expected"
+    log.error.assert_called_once_with("1: syntax error: unexpected token 'Token<ID 1:3 a>'")
